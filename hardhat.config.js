9d4f52f2888090e8c617a8aedf70bc33b6ddb662
Александр Щепетов <cherryknight@yandex.ru>
2022-03-15 17:31:16 +0300

    Load necessary packages

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,7 @@
+require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-waffle");
+require('@openzeppelin/hardhat-upgrades');
+
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -17,5 +20,17 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.3",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1
+          }
+        }
+      }
+    ]
+  },
 };
