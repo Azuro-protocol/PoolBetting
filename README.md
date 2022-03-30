054a007fb05b6984ecdb7bb10848fc0be9e33ed5
Александр Щепетов <cherryknight@yandex.ru>
2022-03-30 16:39:50 +0300

    Rinkeby deploy
    - Add deploy test contract information
    - Import new libraries using while deploy
    - Remove useless network environment variables

--- README.md
+++ README.md
@@ -51,3 +51,24 @@ npm run node
    ```
    where **%network%** is name of network where you would like to upgrade your contract.
 
+## Rinkeby test network
+### Contracts
+```
+TotoBetting  0xAbA4F2EB0ec797937FC71D2Aaf52E37ca19124b0
+Impl         0xb67b09cC3Ca64089693d3Fb8C1DFE7E03CD05115
+USDT         0x1Fe21eaDf6AA833e306D28F17581283Ba52c8d9E
+```
+### Test accounts
+```
+Owner/Oracle 0x5089e2dfad395c517dbce76af3f2491eab40174f
+PrivKey      2dd407af9978a25e03a3459591a0f942a0b979935dc96326b25ea1682e56ec6f
+
+Wallet1      0x20293b57ce29340C34e65324919F83782Fb69c6a
+PrivKey1     d9609229d953280f3fb071c7ac99597769b231618e74f5c6b5ce538a29b23aff
+
+Wallet2      0xE7Cd5b8E0Ea1E4136BE09DA14b94a9C3b8454E5F
+PrivKey2     7f1e0261cf7f60575e57f391a731c6447c71d34823e19a65e2c09a360aa3de13
+
+Wallet3      0x3cbd62EE6De8302F41fCFfF5852F35715265BaEF
+PrivKey3     b928bb5e371e10b77f4fb81a515cb6a5b6fb7881d3f96caa585bda7428921efd
+```
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,7 @@
 require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
 require("@nomiclabs/hardhat-waffle");
-require("@openzeppelin/hardhat-upgrades");
+require('@openzeppelin/hardhat-upgrades');
 require("hardhat-contract-sizer");
 require("hardhat-docgen");
 require("hardhat-gas-reporter");
@@ -8,16 +9,9 @@ require("hardhat-gas-reporter");
 require("dotenv").config();
 
 const ALCHEMY_API_KEY_RINKEBY = process.env.ALCHEMY_API_KEY_RINKEBY || "";
-const ALCHEMY_API_KEY_KOVAN = process.env.ALCHEMY_API_KEY_KOVAN || "";
-const KOVAN_PRIVATE_KEY = process.env.KOVAN_PRIVATE_KEY || "";
 const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY || "";
-const MAINNET_PRIVATE_KEY = process.env.MAINNET_PRIVATE_KEY || "";
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
-const BSC_PRIVATE_KEY = process.env.BSC_PRIVATE_KEY || "";
-const SOKOL_PRIVATE_KEY = process.env.SOKOL_PRIVATE_KEY || "";
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
@@ -25,9 +19,6 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
     console.log(account.address);
   }
 });
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
 
 const exportNetworks = {
   hardhat: {},
@@ -37,29 +28,14 @@ const exportNetworks = {
     defaultBalanceEther: 10,
   },
 };
-if (ALCHEMY_API_KEY_KOVAN != "" && KOVAN_PRIVATE_KEY != "") {
-  exportNetworks["kovan"] = {
-    url: `https://eth-kovan.alchemyapi.io/v2/${ALCHEMY_API_KEY_KOVAN}`,
-    accounts: [`${KOVAN_PRIVATE_KEY}`],
-  };
-}
+
 if (ALCHEMY_API_KEY_RINKEBY != "" && RINKEBY_PRIVATE_KEY != "") {
   exportNetworks["rinkeby"] = {
     url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_API_KEY_RINKEBY}`,
     accounts: [`${RINKEBY_PRIVATE_KEY}`],
   };
 }
-if (SOKOL_PRIVATE_KEY != "") {
-  exportNetworks["sokol"] = {
-    url: `https://sokol.poa.network/`,
-    accounts: [`${SOKOL_PRIVATE_KEY}`],
-    gasPrice: 7000000000,
-  };
-}
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: {
     compilers: [
@@ -73,6 +49,13 @@ module.exports = {
         },
       },
     ],
+  },
+  defaultNetwork: "hardhat",
+  networks: exportNetworks,
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: ETHERSCAN_API_KEY
   },
   contractSizer: {
     alphaSort: true,
